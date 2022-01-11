883b96ca0ac489fac1f45a275eec148ed0ca2dd3
Tim Grünert <tim.gruenert@htw-dresden.de>
2022-01-11 16:30:18 +0100

    first header and intro text

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            siteLogo: './assets/images/bookshop-logo.png',
+            cartLogo: './assets/images/shopping-cart.png',
+            search: '',
+        }
+    }
+})
